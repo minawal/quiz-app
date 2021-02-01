5c411b91ba1e8970e680a8f37cabcfaae982067c
Abdullah Minawal <abdullah.minawal@hotmail.de>
2021-02-01 15:38:56 +0100

    Update navbar

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,86 +1,23 @@
+import getAllByDataJs from './getAllByDataJs'
 import getByDataJs from './getByDataJs'
 
 export default function navigation() {
-  const headerHome = getByDataJs('header-home')
-  const headerBookmarks = getByDataJs('header-bookmarks')
-  const headerCreate = getByDataJs('header-create')
-  const headerProfile = getByDataJs('header-profile')
+  const pages = getAllByDataJs('page')
+  const navButtons = getAllByDataJs('nav-button')
 
-  const buttonHome = getByDataJs('button-home')
-  const buttonBookmarks = getByDataJs('button-bookmarks')
-  const buttonCreate = getByDataJs('button-create')
-  const buttonProfile = getByDataJs('button-profile')
-
-  const pageHome = getByDataJs('home')
-  const pageBookmarks = getByDataJs('bookmarks')
-  const pageCreate = getByDataJs('create')
-  const pageProfile = getByDataJs('profile')
-
-  buttonHome.addEventListener('click', () => {
-    pageHome.classList.remove('hidden')
-    pageBookmarks.classList.add('hidden')
-    pageCreate.classList.add('hidden')
-    pageProfile.classList.add('hidden')
-
-    headerHome.classList.remove('hidden')
-    headerBookmarks.classList.add('hidden')
-    headerCreate.classList.add('hidden')
-    headerProfile.classList.add('hidden')
-
-    buttonHome.classList.add('navigation__underline')
-    buttonBookmarks.classList.remove('navigation__underline')
-    buttonCreate.classList.remove('navigation__underline')
-    buttonProfile.classList.remove('navigation__underline')
-  })
-
-  buttonBookmarks.addEventListener('click', () => {
-    pageHome.classList.add('hidden')
-    pageBookmarks.classList.remove('hidden')
-    pageCreate.classList.add('hidden')
-    pageProfile.classList.add('hidden')
-
-    headerHome.classList.add('hidden')
-    headerBookmarks.classList.remove('hidden')
-    headerCreate.classList.add('hidden')
-    headerProfile.classList.add('hidden')
-
-    buttonHome.classList.remove('navigation__underline')
-    buttonBookmarks.classList.add('navigation__underline')
-    buttonCreate.classList.remove('navigation__underline')
-    buttonProfile.classList.remove('navigation__underline')
-  })
-
-  buttonCreate.addEventListener('click', () => {
-    pageHome.classList.add('hidden')
-    pageBookmarks.classList.add('hidden')
-    pageCreate.classList.remove('hidden')
-    pageProfile.classList.add('hidden')
-
-    headerHome.classList.add('hidden')
-    headerBookmarks.classList.add('hidden')
-    headerCreate.classList.remove('hidden')
-    headerProfile.classList.add('hidden')
-
-    buttonHome.classList.remove('navigation__underline')
-    buttonBookmarks.classList.remove('navigation__underline')
-    buttonCreate.classList.add('navigation__underline')
-    buttonProfile.classList.remove('navigation__underline')
-  })
-
-  buttonProfile.addEventListener('click', () => {
-    pageHome.classList.add('hidden')
-    pageBookmarks.classList.add('hidden')
-    pageCreate.classList.add('hidden')
-    pageProfile.classList.remove('hidden')
-
-    headerHome.classList.add('hidden')
-    headerBookmarks.classList.add('hidden')
-    headerCreate.classList.add('hidden')
-    headerProfile.classList.remove('hidden')
-
-    buttonHome.classList.remove('navigation__underline')
-    buttonBookmarks.classList.remove('navigation__underline')
-    buttonCreate.classList.remove('navigation__underline')
-    buttonProfile.classList.add('navigation__underline')
+  navButtons.forEach(button => {
+    const clickedButtonName = button.dataset.name // <button data-js="nav" data-name="home">Home</button>
+    button.addEventListener('click', () => {
+      pages.forEach(page => {
+        const pageName = page.dataset.name // <section data-js="page" data-name="home">...</section>
+        page.classList.toggle('hidden', clickedButtonName !== pageName)
+      })
+      navButtons.forEach(button => {
+        button.classList.toggle(
+          'navigation__underline',
+          clickedButtonName === button.dataset.name
+        )
+      })
+    })
   })
 }
